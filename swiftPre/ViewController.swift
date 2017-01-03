0d5a4c70292ef178ba499d635cf5ba9394b9afb5
victor <victor@victordeMacBook-Pro.local>
2017-01-03 21:10:42 +0800

    Initial Commit

---
+++ swiftPre/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftPre
+//
+//  Created by victor on 2017/1/3.
+//  Copyright © 2017年 victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
